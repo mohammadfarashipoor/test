aeca86c8ad91d452a82400e3cce409da37d7cef2
MeeM <77319747+mohammadfarashipoor@users.noreply.github.com>
2024-07-22 20:50:44 +0330

    Add files via upload

---
+++ src/assets/icons/icons.ts
@@ -0,0 +1,64 @@
+import Add from "./Add.svg";
+import Archive from "./archive.svg";
+import Arrow from "./Arrow.svg";
+import Calendar from "./Calendar.svg";
+import CalendarActive from "./Calendar-Active.svg";
+import Close from "./close.svg";
+import Column from "./column.svg";
+import ColumnActive from "./column-Active.svg";
+import Delete from "./Delete.svg";
+import Edit from "./Edit.svg";
+import Exit from "./Exit.svg";
+import Filter from "./Filter.svg";
+import List from "./List.svg";
+import ListActive from "./List-Active.svg";
+import More from "./More.svg";
+import Plus from "./Plus.svg";
+import PlusWhite from "./Plus-white.svg";
+import Search from "./search-loupe.svg";
+import Share from "./Share.svg";
+import Tick from "./Tick.svg";
+import Description from "./Description.svg";
+import WhiteShare from "./WhiteShare.svg";
+import LinkCopy from "./LinkCopy.svg";
+import RedBin from "./RedBin.svg";
+import EditColor from "./EditColor.svg";
+import CarrotCircleDown from "./CarrotCircleDown.svg";
+import Down from "./Down.svg";
+import AddBrand from "./AddBrand.svg";
+import Refresh from "./refresh.svg";
+export {
+    Add,
+    Archive,
+    Refresh,
+    Arrow,
+    Calendar,
+    CalendarActive,
+    AddBrand,
+    Close,
+    Column,
+    ColumnActive,
+    Delete,
+    Edit,
+    Exit,
+    Filter,
+    List,
+    ListActive,
+    More,
+    Plus,
+    PlusWhite,
+    Search,
+    Share,
+    Tick,
+    Description,
+    WhiteShare,
+    LinkCopy,
+    RedBin,
+    EditColor,
+    CarrotCircleDown,
+    Down,
+};
+
+// how to use:
+// step1 => import to ypur file : import * as Icon from "..."
+// step2 => use them everywhere as shown : src = {Icon.Plus}
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@context": path.resolve(__dirname, "./src/context"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@layouts": path.resolve(__dirname, "./src/layouts"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@types": path.resolve(__dirname, "./src/types"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@services": path.resolve(__dirname, "./src/services"),
+      "@constants": path.resolve(__dirname, "./src/constants"),
+    },
+  },
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+});
